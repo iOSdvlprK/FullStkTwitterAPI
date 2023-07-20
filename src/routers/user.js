000f7c7dbb629a7cdc68a4b0cdacb03f99a8def9
iOS developer K <iOSdvlprK@gmail.com>
2023-07-20 22:57:21 +0900

    make a route for creating and fetching users

---
+++ src/routers/user.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const User = require('../models/user');
+
+// original router
+const router = new express.Router();
+
+// endpoints
+
+// create a new user
+router.post('/users', async (req, res) => {
+  const user = new User(req.body)
+
+  try {
+    await user.save()
+    res.status(201).send(user)
+  }
+  catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+// fetch the users
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.send(users)
+  }
+  catch(e) {
+    res.status(500).send(e)
+  }
+})
+
+module.exports = router
