ad8dd08d8f728c374ffcfc425a9a1ea22f4fc22b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-08-02 23:44:01 +0900

    upload tweet image

--- src/routers/tweet.js
+++ src/routers/tweet.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const Tweet = require('../models/tweet')
 
 // new router
 const router = new express.Router()
 
 const auth = require('../middleware/auth')
+
+// helper functions
+const upload = multer({
+  limits: {
+    fileSize: 100000000
+  }
+})
 
 // post tweet router
 router.post('/tweets', auth, async (req, res) => {
@@ -20,6 +29,23 @@ router.post('/tweets', auth, async (req, res) => {
   catch (err) {
     res.status(400).send(err)
   }
+})
+
+// add image to tweet route
+router.post('/uploadTweetImage/:id', auth, upload.single('upload'), async (req, res) => {
+  const tweet = await Tweet.findOne({ _id: req.params.id })
+  console.log(tweet)
+
+  if (!tweet) {
+    throw new Error('Cannot find the tweet')
+  }
+
+  const buffer = await sharp(req.file.buffer).resize({width: 350, height: 350}).png().toBuffer()
+  tweet.image = buffer
+  await tweet.save()
+  res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
 })
 
 // fetch all tweets
