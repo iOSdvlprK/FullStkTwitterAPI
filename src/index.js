1c4d96dc76f452848dd6f6944d75d198e227e055
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-08-06 23:21:11 +0900

    Notification router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const tweetRouter = require('./routers/tweet')
+const notificationRouter = require('./routers/notification')
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(userRouter)
 app.use(tweetRouter)
+app.use(notificationRouter)
 
 app.listen(port, () => {
   console.log('Server is up on the port ' + port)
